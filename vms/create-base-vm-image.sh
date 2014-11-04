ab02d118748438acfd6160110c489367f56cd1d3
Rich Wareham <rjw57@cam.ac.uk>
2014-11-04 20:24:31 +0000

    vms: don't launch VNC console on virtual machine creation

--- vms/create-base-vm-image.sh
+++ vms/create-base-vm-image.sh
@@ -8,8 +8,7 @@ set -xe
 virt-install --virt-type kvm --name debian-7 --ram 1024				\
 	--location=http://ftp.debian.org/debian/dists/wheezy/main/installer-amd64/	\
 	--disk path=/tmp/debian-7-amd64-vm.img,size=5 --network network=default		\
-	--graphics vnc,listen=0.0.0.0 --noautoconsole --os-type=linux			\
-	--initrd-inject=preseed.cfg							\
+	--noautoconsole --os-type=linux --initrd-inject=preseed.cfg			\
 	--os-variant=debianwheezy --extra-args="priority=critical interface=auto	\
 		debian-installer/language=en debian-installer/country=GB		\
 		debian-installer/locale=en_GB keymap=gb console=ttyS0,115200n8"
